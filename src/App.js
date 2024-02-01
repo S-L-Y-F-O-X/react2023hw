f2d833ef0943b3b64f95ae57ae47b0e779eac762
Sly Fox <Alexnkest@gmail.com>
2024-02-01 15:32:06 +0200

    Hw 1 Simpsons + Rick and Morty

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import {Simpsons} from "./components/Simpsons/Simpsons";
+import {Characters} from "./components/RickAndMorty/Characters";
 
 const App = () => {
     return (
         <div>
+            <h1>Simpson family</h1>
             <Simpsons/>
+            <h1>Characters "Rick and Morty"</h1>
+            <Characters/>
         </div>
     );
 };
--- src/components/RickAndMorty/Character.js
+++ src/components/RickAndMorty/Character.js
@@ -1,10 +1,12 @@
-const Simpson = ({simpson}) => {
-    const {name, age, gender, image} = simpson;
+const Character = ({character}) => {
+    const {name,status,species,gender,image} = character;
+
     return (
         <div>
             <hr/>
             <div>Name: {name}</div>
-            <div>Age: {age}</div>
+            <div>Status: {status}</div>
+            <div>Species: {species}</div>
             <div>Gender: {gender}</div>
             <img src={image} alt={name}/>
             <hr/>
@@ -12,4 +14,4 @@ const Simpson = ({simpson}) => {
     );
 };
 
-export {Simpson};
+export {Character};
